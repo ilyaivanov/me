06ff6036044c4c1bc3ce4d7f306ea36b9987a1e1
Ilya Ivanov <static.ila@gmail.com>
2020-12-23 15:05:55 +0200

    Migrate to a new functions host

--- src/api/youtubeRequest.ts
+++ src/api/youtubeRequest.ts
@@ -1,62 +1,58 @@
+const API_HOST = "https://europe-west1-slapstuk.cloudfunctions.net";
+
 export const findYoutubeVideos = (
   term: string,
   pageToken?: string
 ): Promise<any> => {
-  if (process.env.NODE_ENV === "test")
-    throw new Error("Tried to execute real API call from tests");
-  else {
-    let url = `https://europe-west1-lean-watch.cloudfunctions.net/getVideos?q=${term}`;
-    if (pageToken) url += `&pageToken=${pageToken}`;
-    return fetch(url).then((res) => res.json());
-  }
+  verifyNonTextEnvironment();
+  let url = `${API_HOST}/getVideos?q=${term}`;
+  if (pageToken) url += `&pageToken=${pageToken}`;
+  return fetch(url).then((res) => res.json());
 };
 
 export const fetchPlaylistVideos = (
   playlistId: string,
   pageToken?: string
 ): Promise<any> => {
-  if (process.env.NODE_ENV === "test")
-    throw new Error("Tried to execute real API call from tests");
-  else {
-    let url = `https://europe-west1-lean-watch.cloudfunctions.net/getPlaylistItems?playlistId=${playlistId}`;
+  verifyNonTextEnvironment();
+  let url = `${API_HOST}/getPlaylistItems?playlistId=${playlistId}`;
 
-    if (pageToken) url += `&pageToken=${pageToken}`;
-    return fetch(url).then((res) => res.json());
-  }
+  if (pageToken) url += `&pageToken=${pageToken}`;
+  return fetch(url).then((res) => res.json());
 };
 
 export const getChannelPlaylists = (channelId: string, pageToken?: string) => {
-  if (process.env.NODE_ENV === "test")
-    throw new Error("Tried to execute real API call from tests");
-  else {
-    let url = `https://europe-west1-lean-watch.cloudfunctions.net/getChannelPlaylists?part=snippet&channelId=${channelId}`;
-    if (pageToken) url += `&pageToken=${pageToken}`;
-    return fetch(url).then((res) => res.json());
-  }
+  verifyNonTextEnvironment();
+  let url = `${API_HOST}/getChannelPlaylists?part=snippet&channelId=${channelId}`;
+  if (pageToken) url += `&pageToken=${pageToken}`;
+  return fetch(url).then((res) => res.json());
 };
 
 export const getChannelUploadsPlaylistId = (
   channelId: string
 ): Promise<string> => {
-  if (process.env.NODE_ENV === "test")
-    throw new Error("Tried to execute real API call from tests");
-  else {
-    let url = `https://europe-west1-lean-watch.cloudfunctions.net/getChannelVideos?channelId=${channelId}`;
-    return fetch(url)
-      .then((res) => res.json())
-      .then((res) => {
-        console.log(res);
-        return res.playlistId;
-      });
-  }
+  verifyNonTextEnvironment();
+  let url = `${API_HOST}/getChannelVideos?channelId=${channelId}`;
+  return fetch(url)
+    .then((res) => res.json())
+    .then((res) => {
+      console.log(res);
+      return res.playlistId;
+    });
 };
 
 export const findSimilarYoutubeVideos = (
   videoId: string,
   pageToken?: string
 ) => {
-  let url = `https://europe-west1-lean-watch.cloudfunctions.net/getVideos?relatedToVideoId=${videoId}&type=video`;
+  verifyNonTextEnvironment();
+  let url = `${API_HOST}/getVideos?relatedToVideoId=${videoId}&type=video`;
 
   if (pageToken) url += `&pageToken=${pageToken}`;
   return fetch(url).then((res) => res.json());
+};
+
+const verifyNonTextEnvironment = () => {
+  if (process.env.NODE_ENV === "test")
+    throw new Error("Tried to execute real API call from tests");
 };
